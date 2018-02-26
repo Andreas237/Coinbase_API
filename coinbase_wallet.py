8de3ad5e8ca01a5a5d5e0069d0b87cb7d8163644
Andreas237 <silenta237@gmail.com>
2018-02-26 17:51:59 +1100

    working on pricing logic.  https://developers.coinbase.com/api/v2#get-buy-price

--- coinbase_wallet.py
+++ coinbase_wallet.py
@@ -48,18 +48,71 @@ class cb:
 			datas = json.loads( f.read() )
 		self.api_key=datas['key']
 		self.api_secret=datas['secret']
-	
- 
-	
 
-	
-	
-	
-	def __init__(self):
+
+
+
+	# Get accounts associated with the key:secret
+	def getPrimaryAccounts(self):
+		self.primaryAccount = primaryAccount = self.client.get_primary_account() 
+		print("%s: %s %s" % ( primaryAccount.name, primaryAccount.balance.amount, primaryAccount.balance.currency ) )
+
+
+
+
+	# Check market mismatch on prices
+	def checkMarket(self):
+		currency_pair = 'BTC-USD'
+		buyP = self.client.get_buy_price(currency_pair = currency_pair ).amount
+		sellP = self.client.get_sell_price(currency_pair = currency_pair ).amount
+		if buyP > sellP :
+			print("BUYERS PAYING %s above asking!\n" % ( str(buyP - sellP)  )
+		elif buyP < sellP:
+			print("SELLERS ASKING %s above buy!\n" % ( str(sellP - buyP) ) )
+		# end else-if
+		else:
+			print("The market is at parity...: BUY %s == SELL %s"  % (buyP, sellP) )
+		# end else
+
+
+
+
+	# Get the real-time price of BTC
+	def rt_price(self):
+		price = self.client.get_spot_price(currency=self.currency_code)
+		return price.amount
+
+
+
+
+	# run a command for a little while
+	def run(self):
+		count = 10
+		while( count != 0 ):
+			self.checkMarket()
+			count = count - 1
+			time.sleep(5)
+
+
+
+
+
+
+	# Setup credits and self vars
+	def setup(self):
 		self.getApiCreds()
 		self.authenticate( self.api_key, self.api_secret)
-		self.getAccounts()
+		self.primaryAccount = self.client.get_primary_account()
+		self.currency_code = 'USD'
+
+
+
+
+	def __init__(self):
+		self.setup()
+		print("Current price in %s: %s" % ( self.currency_code, self.rt_price()  ))
+		#print("Payment methods: %s" % ( self.client.get_payment_methods() ) )
+		self.checkMarket()
 		print("End of class")
 
 x = cb()
-json.dumps("creds.json")
